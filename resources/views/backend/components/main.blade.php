aeaf1ce02ca0fcace82f10f1cf0a6becae7ecb57
Zulfiker <zulfikerhossain2023@gmail.com>
2025-09-18 20:38:03 +0600

    update

--- resources/views/backend/components/main.blade.php
+++ resources/views/backend/components/main.blade.php
@@ -14,7 +14,7 @@
                  <div class="row">
                      <div class="form-group col-md-4 mt-3">
                          <h3>Background Image</h3>
-                         <img id="bc_img_preview" style="height: 30vh; display: block" src="{{ url($main->bc_img) }}"
+                         <img id="bc_img_preview" style="height: 30vh; display: block" src="{{ asset($main->bc_img ?? null) }}"
                              class="img-thumbnail">
                          <input class="mt-3" type="file" id="bc_img" name="bc_img">
                      </div>
@@ -25,19 +25,19 @@
                              </label>
 
                              <input type="text" class="form-control" id="title" name="title"
-                                 value="{{ $main->title }}">
+                                 value="{{ $main?->title ?? ""}}">
                          </div>
                          <div class="mb-5">
                              <label for="sub_title">
                                  <h4>Sub Title</h4>
                              </label>
                              <input type="text" class="form-control" id="sub_title" name="sub_title"
-                                 value="{{ $main->sub_title }}">
+                                 value="{{ $main?->sub_title ?? ""}}">
                          </div>
                          <div>
                              <h4>Upload Resume</h4>
                              <input class="mt-2" type="file" id="resume" name="resume"
-                                 value="{{ url($main->resume) }}">
+                                 value="{{ asset($main?->resume) }}">
                          </div>
                      </div>
                  </div>
