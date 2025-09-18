f82309fd199a6560ac0211d31f4f9b3a0ae65c1e
Zulfiker <zulfikerhossain2023@gmail.com>
2025-09-18 20:26:43 +0600

    update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -39,16 +39,31 @@ class MainController
         $main->title = $request->title;
         $main->sub_title = $request->sub_title;
 
-        if ($request->file('bc_img')) {
+        // if ($request->file('bc_img')) {
+        //     $img_file = $request->file('bc_img');
+        //     $img_file->storeAs('public/img/', 'bc_img.' . $img_file->getClientOriginalExtension());
+        //     $main->bc_img = 'storage/img/bc_img.' . $img_file->getClientOriginalExtension();
+        // }
+
+        // if ($request->file('resume')) {
+        //     $pdf_file = $request->file('resume');
+        //     $pdf_file->storeAs('public/pdf/', 'resume.' . $pdf_file->getClientOriginalExtension());
+        //     $main->resume = 'storage/pdf/resume.' . $pdf_file->getClientOriginalExtension();
+        // }
+
+
+        if ($request->hasFile('bc_img')) {
             $img_file = $request->file('bc_img');
-            $img_file->storeAs('public/img/', 'bc_img.' . $img_file->getClientOriginalExtension());
-            $main->bc_img = 'storage/img/bc_img.' . $img_file->getClientOriginalExtension();
+            $img_name = 'bc_img.' . $img_file->getClientOriginalExtension();
+            $img_file->move(public_path('img'), $img_name); // move to public/img
+            $main->bc_img = 'img/' . $img_name;
         }
 
-        if ($request->file('resume')) {
+        if ($request->hasFile('resume')) {
             $pdf_file = $request->file('resume');
-            $pdf_file->storeAs('public/pdf/', 'resume.' . $pdf_file->getClientOriginalExtension());
-            $main->resume = 'storage/pdf/resume.' . $pdf_file->getClientOriginalExtension();
+            $pdf_name = 'resume.' . $pdf_file->getClientOriginalExtension();
+            $pdf_file->move(public_path('pdf'), $pdf_name); // move to public/pdf
+            $main->resume = 'pdf/' . $pdf_name;
         }
 
         $main->save();
--- resources/views/frontend/components/banner.blade.php
+++ resources/views/frontend/components/banner.blade.php
@@ -1,8 +1,9 @@
-        <!-- Masthead-->
-        <header class="masthead" style="background-image: url('{{ url($main->bc_img) }}')">
-            <div class="container">
-                <div class="masthead-subheading">{{ $main->title }}</div>
-                <div class="masthead-heading text-uppercase">{{ $main->sub_title }}</div>
-                <a class="btn btn-primary btn-xl text-uppercase" href="{{ url($main->resume) }}">Download Resume</a>
-            </div>
-        </header>
+              <!-- Masthead-->
+              <header class="masthead" style="background-image: url('{{ asset($main?->bc_img ?? null) }}')">????
+                  <div class="container">
+                      <div class="masthead-subheading">{{ $main?->title }}</div>
+                      <div class="masthead-heading text-uppercase">{{ $main?->sub_title }}</div>
+                      <a class="btn btn-primary btn-xl text-uppercase" href="{{ asset($main?->resume) }}">Download
+                          Resume</a>
+                  </div>
+              </header>
